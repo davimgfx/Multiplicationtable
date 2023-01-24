56cdf0b8287b3672c592c5ec3e3bc6b6706d6653
Davi Costa <118557337+davimgfx@users.noreply.github.com>
2023-01-24 10:17:57 -0300

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,15 @@
+function startCount(){
+    var num = document.getElementById("number")
+    if(Number(num.value) == 0){
+        window.alert("I need a number!!!!!!")
+    }
+    else{
+        document.getElementById("answer").style.cssText = 'height: 450px;' +
+'width: 400px;' + "margin: auto;" + "border-radius: 10px;"
++ "box-shadow: 2px 2px 2px black;" + "border: 5px solid #998eee;" + "font-size: 15px;" + "font-weight: bold;" + "padding: 20px;"
+        document.getElementById("answer").innerHTML = "MULTIPLICATION TABLE <br> <br> "
+        for(let i = 0; i <= 10; i++){
+            document.getElementById("answer").innerHTML += `${i} x ${Number(num.value)} = ${Number(num.value)*i} <br> <br>`
+        }
+    }
+}
